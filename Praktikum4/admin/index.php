ab3f73799959c5176917aa2de22e99f904250cea
muhammadvirgizulfahmi <muha23126ti@student.nurulfikri.ac.id>
2024-03-16 10:32:44 +0700

    praktikum 4

---
+++ Praktikum4/admin/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'navbar.html';
+require_once 'sidebar.html';
+?>
+
+<h2>Selamat Datang Admin</h2>
+
+<?php
+require_once 'footer.html';
+?>
---
+++ Praktikum4/admin/proses_registrasi.php
@@ -0,0 +1,33 @@
+<?php
+    $program_studi = [
+        "SI"=>"Sistem Informasi", 
+        "TI"=>"Teknik Informatika", 
+        "BD"=>"Bisnis Digital"];
+    $skill = [
+        "HTML"=>10, 
+        "CSS"=>10, 
+        "JavaScript"=>20, 
+        "RWD Bootstrap"=>20,
+        "PHP"=>30, 
+        "Python"=>30, 
+        "Java"=>50];
+    $domisili = ["Jakarta", "Bogor", "Depok", "Tanggerang"];
+?>
+
+<?php /*
+    $skill_pilihan = $_POST['skills'];
+    // cek hasilnya
+    // nilai array $_skill_pilihan ini yang nanti digunakan untuk hitung total
+    print_r($skill_pilihan);
+?>
+<?php /*
+foreach($ar_skill as $k => $v){
+    echo '<input type="checkbox" name="skills[]" value="'.$k.'">'.$k;
+}
+?>
+
+<?php
+foreach($ar_prodia as $k => $v){
+    echo '<option value="' .$k.'">'.$v.'</option>';
+} */
+?>
